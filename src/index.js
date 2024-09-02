b38b0763e1e0e23b171ad610902ad8ecd1ba35e2
rjsaimon-gif <sreginajilta@gmail.com>
2024-09-02 16:48:41 -0500

    Added the basics to the JS file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert ("Hello Everyone!")
+function generateRecipe(event) {
+  event.preventDefault();
+
+  new Typewriter("#recipe", {
+    strings: ["12 amazing salads to compliment your meal"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
