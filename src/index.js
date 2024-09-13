8f758fde93af06aec961700130f8081128fa6129
rjsaimon-gif <sreginajilta@gmail.com>
2024-09-13 18:04:26 -0500

    Made updates to the JS and CSS file to enhance the user's experience

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function displayRecipe(response) {
   new Typewriter("#recipe", {
     strings: [response.data.answer],
     autoStart: true,
-    delay: 75,
+    delay: 1,
     cursor: "",
   });
 }
@@ -14,10 +14,14 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "7a8aacadf32fc08tff87b375486o409c";
   let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
-  let context = `You are a reliable recipe expert. your mission is to generate a recipe in basic HTML. Be sure to follow the user's instructions.`;
+  let context = `You are a reliable recipe expert. your mission is to generate a recipe following the user's instructions. please be sure to write in HTML format.`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(
     prompt
   )}&context=${encodeURIComponent(context)}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `⏳ Generating a recipe about ${instructionsInput.value}`;
 
   console.log("generating recipe");
   console.log("Prompt: ${prompt}");
@@ -26,5 +30,5 @@ function generateRecipe(event) {
   axios.get(apiURL).then(displayRecipe);
 }
 
-let recipeFormElement = document.querySelector("#recipe-generator-form");
-recipeFormElement.addEventListener("submit", generateRecipe);
+let recipeElement = document.querySelector("#recipe-generator-form");
+recipeElement.addEventListener("submit", generateRecipe);
